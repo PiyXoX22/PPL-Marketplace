07b33d35aa7abbb77c1934f4d9b3510201cb30c7
PiyXoX Developer <ilhamandikaputra225@gmail.com>
2025-11-17 12:52:59 +0700

    ui cart and profile

--- routes/web.php
+++ routes/web.php
@@ -4,16 +4,27 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\QtyController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UpuiController;
 use App\Http\Controllers\HargaController;
 use App\Http\Controllers\GambarController;
 use App\Http\Controllers\ProdukController;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\KategoriController;
-use App\Http\Controllers\UpuiController;
 
 // Tampilan User
 Route::get('/', function () {
     return view('site.index');
 })->name('home');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/profile', [ProfileController::class, 'index'])->name('profile.index');
+    Route::post('/profile/update', [ProfileController::class, 'update'])->name('profile.update');
+});
+
+Route::get('/cart', function () {
+    return view('cart.index');
+})->name('cart.index');
+
 
 // Tampilan & CRUD Admin
 Route::get('/dashboard', function () {
